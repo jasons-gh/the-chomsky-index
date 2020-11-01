0c0bdee7059d02d80e43911b51aba443721f818a
Jason Lobo <jlobo96@outlook.com>
2020-11-01 19:29:54 +0000

    Convert from SRT to CNT in base.py

--- base.py
+++ base.py
@@ -1,3 +1,4 @@
+import convert
 import sources
 import subprocess
 from pathlib import Path
@@ -5,21 +6,17 @@ from unidecode import unidecode
 
 
 # youtube-dl - download the subtitles as .vtt files
-
 urls_yt = sources.urls_yt()
-
 for url in urls_yt:
     subprocess.run(['youtube-dl', '-i', '--skip-download', '--write-auto-sub', '-o%(title)s %(id)s', url])
 
 
-# ffmpeg - convert them from .vtt to .srt
-
+# ffmpeg - convert from .vtt to .srt
 for file in [x for x in Path(__file__).parent.glob('**/*.vtt') if x.is_file()]:
     subprocess.run('ffmpeg.exe -i "' + str(file.name) + '" -- "' + str(file.name[:-4]) + '".srt')
 
 
 # unidecode - replace some characters if present
-
 unidecoded_files = set()
 
 for file in [x for x in Path(__file__).parent.glob('**/*.srt') if x.is_file()]:
@@ -34,8 +31,10 @@ for file in [x for x in Path(__file__).parent.glob('**/*.srt') if x.is_file()]:
         file_object.write(data)
 
 
-# information
+# convert - convert from .srt to .cnt
+convert.convert()
 
+# information
 print(str(len(urls_yt)) + ' subtitle files requested')
 print(str(len([x for x in Path(__file__).parent.glob('**/*.vtt') if x.is_file()])) + ' .vtt subtitle files in this folder')
 print(str(len([x for x in Path(__file__).parent.glob('**/*.srt') if x.is_file()])) + ' .srt subtitle files in this folder')
--- convert.py
+++ convert.py
@@ -8,53 +8,57 @@ from pathlib import Path
 # But sometimes only the first case (end of a pair) exists, which are lines
 # 3 + 10i.
 
-for srt_file in [x for x in Path(__file__).parent.glob('**/*.srt') if x.is_file()]:
-    with open(srt_file, 'r', encoding='utf-8') as file_object:
-        contents = file_object.readlines()
 
-        # Check for repeating lines from auto-generated subtitles (use lines 3+10i)
-        # or non-repeating subtitles from a manual upload (parse using srt library)
-
-        if (
-            len(contents) > 40 and
-            ((contents[3].strip() != '' and contents[3] == contents[7] and contents[7] == contents[12]) or
-            (contents[13].strip() != '' and contents[13] == contents[17] and contents[17] == contents[22]) or
-            (contents[23].strip() != '' and contents[23] == contents[27] and contents[27] == contents[32]) or
-            (contents[33].strip() != '' and contents[33] == contents[37] and contents[37] == contents[42]))
-            ):
-            print(srt_file.name + ' is a repeating file')
-
-            # Make lists of lines and times
-
-            lines = []
-            i = 0
-            while 3+10*i < len(contents):
-                lines.append(contents[3+10*i])
-                i += 1
-
-            times = []
-            i = 0
-            while i < len(lines):
-                t = datetime.strptime(contents[1+10*i][:8], '%H:%M:%S')
-                times.append(str(t.hour*3600 + t.minute*60 + t.second) + '\n')
-                i += 1
-
-        else:
-            print(srt_file.name + ' is a nonrepeating file')            
-            subtitles = pysubs2.load(srt_file, encoding="utf-8")
-            lines = []
-            for i in subtitles:
-                lines.append(str(i.text.replace('\n', ' ').replace(r'\N', ' ')) + '\n')
-            times = []
-            for i in subtitles:
-                times.append(str(int(i.start // 1000)) + '\n')
-
-        # Make .en.cnt with lines then times
-
-        with open(str(Path(__file__).parent) + '\\' + srt_file.name[:-7] + '.en.cnt', 'a') as cnt_file:
-            for i in lines:
-                cnt_file.write(i)
+def convert():
+    for srt_file in [x for x in Path(__file__).parent.glob('**/*.srt') if x.is_file()]:
+        with open(srt_file, 'r', encoding='utf-8') as file_object:
+            contents = file_object.readlines()
     
-        with open(str(Path(__file__).parent) + '\\' + srt_file.name[:-7] + '.en.cnt', 'a') as cnt_file:
-            for i in times:
-                cnt_file.write(str(i))
+            # Check for repeating lines from auto-generated subtitles (use lines 3+10i)
+            # or non-repeating subtitles from a manual upload (parse using srt library)
+    
+            if (
+                len(contents) > 40 and
+                ((contents[3].strip() != '' and contents[3] == contents[7]
+                                            and contents[7] == contents[12]) or
+                (contents[13].strip() != '' and contents[13] == contents[17]
+                                            and contents[17] == contents[22]) or
+                (contents[23].strip() != '' and contents[23] == contents[27]
+                                            and contents[27] == contents[32]) or
+                (contents[33].strip() != '' and contents[33] == contents[37]
+                                            and contents[37] == contents[42]))
+                ):
+                print(srt_file.name + ' is a repeating file')
+
+                lines = []
+                i = 0
+                while 3+10*i < len(contents):
+                    lines.append(contents[3+10*i])
+                    i += 1
+
+                times = []
+                i = 0
+                while i < len(lines):
+                    t = datetime.strptime(contents[1+10*i][:8], '%H:%M:%S')
+                    times.append(str(t.hour*3600 + t.minute*60 + t.second) + '\n')
+                    i += 1
+
+            else:
+                print(srt_file.name + ' is a nonrepeating file')
+                subtitles = pysubs2.load(srt_file, encoding="utf-8")
+                lines = []
+                for i in subtitles:
+                    lines.append(str(i.text.replace('\n', ' ').replace(r'\N', ' ')) + '\n')
+                times = []
+                for i in subtitles:
+                    times.append(str(int(i.start // 1000)) + '\n')
+
+            # Make .en.cnt with lines then times
+
+            with open(str(Path(__file__).parent) + '\\' + srt_file.name[:-7] + '.en.cnt', 'a') as cnt_file:
+                for i in lines:
+                    cnt_file.write(i)
+
+            with open(str(Path(__file__).parent) + '\\' + srt_file.name[:-7] + '.en.cnt', 'a') as cnt_file:
+                for i in times:
+                    cnt_file.write(str(i))
